5608788f3a54b32cd47272fab80da20b867760ab
emeliepetersson <emepet1204@skola.goteborg.se>
2020-04-29 11:11:53 +0200

    Update Contentful.js

--- src/Contentful.js
+++ src/Contentful.js
@@ -5,8 +5,8 @@ const spaceKey = process.env.REACT_APP_CONTENTFUL_SPACE;
 const accessTokenKey = process.env.REACT_APP_CONTENTFUL_ACCESSTOKEN;
 
 const client = contentful.createClient({
-  space: "9s0j51rapmhp",
-  accessToken: "kL8gLxTV38hiWgLJ88fJC86OMmRF-jjiIJinVvVbIbo",
+  space: spaceKey,
+  accessToken: accessTokenKey,
 });
 
 export default client;
